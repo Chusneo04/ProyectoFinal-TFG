924560600752cf0162991bf45981f72642e3b183
Chusneo04 <hugoarroyogalle@gmail.com>
2025-04-27 12:25:23 +0200

    cambios en el register

--- src/app.py
+++ src/app.py
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 from forms import Usuario
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 app = Flask(__name__)
 
@@ -106,19 +110,48 @@ def register():
 
                 cursor.execute('SELECT id, nombre, apellidos, correo, clave, fecha_de_creacion FROM usuarios WHERE correo = %s', (correo,))
                 nuevo_usuario = cursor.fetchone()
-
                 #Y aqui ya llevamos a cabo el inicio de sesión para que pueda acceder a las rutas protegidas
                 if nuevo_usuario:
                     usuario_obj = User(nuevo_usuario[0], nuevo_usuario[1], nuevo_usuario[2], nuevo_usuario[3], nuevo_usuario[4], nuevo_usuario[5])
+                    print(type(nuevo_usuario[3]))
                     login_user(usuario_obj)
+
+                    #Preparamos las credenciales del correo para poder enviar correos
+
+                    servidor_smtp = "smtp.gmail.com"
+                    puerto_smtp = 587
+                    usuario_smtp = "infocurriculum360@gmail.com"
+                    clave_smtp = os.getenv('EMAIL_KEY')
+
+                    #Preparamos el mensaje para enviarlo por correo
+
+                    msg = MIMEMultipart()
+                    msg['From'] = usuario_smtp
+                    msg['To'] = nuevo_usuario[3]
+                    msg['Subject'] = "Bienvenido {}".format(nuevo_usuario[1])
+                    mensaje = "¡Bienvenido/a a Curriculum360! Nos alegra que formes parte de nuestra comunidad.Aquí encontrarás herramientas para mejorar tu perfil profesional, organizar tu experiencia y destacar tu talento. Nos comprometemos a ayudarte en cada paso del camino."
+                    msg.attach(MIMEText(mensaje, "plain")) #Esto introduce el mensaje de la linea superior en el cuerpo del correo
+                    try:
+                        with smtplib.SMTP(servidor_smtp, puerto_smtp) as server:
+                            server.starttls() #Iniciamos la conexion con seguridad 
+                            server.login(usuario_smtp, clave_smtp) #Iniciamos sesion en el correo
+                            server.sendmail(usuario_smtp, msg["To"], msg.as_string()) #Enviamos el mensaje de bienvenida al nuevo usuario
+                            print("Correo enviado correctamente")
+                            return redirect(url_for('perfil')) #Si todo va bien redirigirá al usuario a la interfaz de perfil
+                    except Exception as e:
+                        print('144')
+                        print("Error al enviar correo: {}".format(e))
+                        return render_template('register.html', usuario=usuario)
+
+
                     
-                    return redirect(url_for('perfil')) #Si todo va bien redirigirá al usuario a la interfaz de perfil
-                return render_template('register.html') #Si algo falla te vuelve a mostrar la de registro
+                return render_template('register.html', usuario=usuario) #Si algo falla te vuelve a mostrar la de registro
 
         return render_template('register.html', usuario=usuario) #Muestra la interfaz de registro y envia al html el formulario para gestionar alli el diseño   
 
     except:
         flash('Error al registrar')
+        return render_template('register.html', usuario=usuario)
 
     
 #Esta es la funcion de la interfaz de perfil
